afe287713c45cb7de9a5b0924f1359c84dd72c65
Philip Forget <philipforget@gmail.com>
2013-10-24 15:26:14 -0400

    get rid of migration that was failing on foreign key rename and just start from scratch

--- oauth_provider/migrations/0001_initial.py
+++ oauth_provider/migrations/0001_initial.py
@@ -1,151 +1,153 @@
-# encoding: utf-8
+# -*- coding: utf-8 -*-
 import datetime
 from south.db import db
 from south.v2 import SchemaMigration
-from oauth_provider.compat import AUTH_USER_MODEL
 from django.db import models
+
 
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
-        
         # Adding model 'Nonce'
-        db.create_table('oauth_provider_nonce', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+        db.create_table(u'oauth_provider_nonce', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('token_key', self.gf('django.db.models.fields.CharField')(max_length=32)),
             ('consumer_key', self.gf('django.db.models.fields.CharField')(max_length=256)),
             ('key', self.gf('django.db.models.fields.CharField')(max_length=255)),
+            ('timestamp', self.gf('django.db.models.fields.PositiveIntegerField')(db_index=True)),
         ))
-        db.send_create_signal('oauth_provider', ['Nonce'])
+        db.send_create_signal(u'oauth_provider', ['Nonce'])
 
-        # Adding model 'Resource'
-        db.create_table('oauth_provider_resource', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+        # Adding model 'Scope'
+        db.create_table(u'oauth_provider_scope', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('name', self.gf('django.db.models.fields.CharField')(max_length=255)),
             ('url', self.gf('django.db.models.fields.TextField')(max_length=2083)),
             ('is_readonly', self.gf('django.db.models.fields.BooleanField')(default=True)),
         ))
-        db.send_create_signal('oauth_provider', ['Resource'])
+        db.send_create_signal(u'oauth_provider', ['Scope'])
 
         # Adding model 'Consumer'
-        db.create_table('oauth_provider_consumer', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+        db.create_table(u'oauth_provider_consumer', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('name', self.gf('django.db.models.fields.CharField')(max_length=255)),
-            ('description', self.gf('django.db.models.fields.TextField')()),
+            ('description', self.gf('django.db.models.fields.TextField')(blank=True)),
             ('key', self.gf('django.db.models.fields.CharField')(max_length=256)),
             ('secret', self.gf('django.db.models.fields.CharField')(max_length=16, blank=True)),
             ('status', self.gf('django.db.models.fields.SmallIntegerField')(default=1)),
-            ('user', self.gf('django.db.models.fields.related.ForeignKey')(to=orm[AUTH_USER_MODEL], null=True, blank=True)),
+            ('user', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'], null=True, blank=True)),
+            ('xauth_allowed', self.gf('django.db.models.fields.BooleanField')(default=False)),
         ))
-        db.send_create_signal('oauth_provider', ['Consumer'])
+        db.send_create_signal(u'oauth_provider', ['Consumer'])
 
         # Adding model 'Token'
-        db.create_table('oauth_provider_token', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+        db.create_table(u'oauth_provider_token', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('key', self.gf('django.db.models.fields.CharField')(max_length=32, null=True, blank=True)),
             ('secret', self.gf('django.db.models.fields.CharField')(max_length=16, null=True, blank=True)),
             ('token_type', self.gf('django.db.models.fields.SmallIntegerField')()),
-            ('timestamp', self.gf('django.db.models.fields.IntegerField')(default=1327884735L)),
+            ('timestamp', self.gf('django.db.models.fields.IntegerField')(default=1382642717L)),
             ('is_approved', self.gf('django.db.models.fields.BooleanField')(default=False)),
-            ('user', self.gf('django.db.models.fields.related.ForeignKey')(blank=True, related_name='tokens', null=True, to=orm[AUTH_USER_MODEL])),
+            ('user', self.gf('django.db.models.fields.related.ForeignKey')(blank=True, related_name='oauth_tokens', null=True, to=orm['auth.User'])),
             ('consumer', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['oauth_provider.Consumer'])),
-            ('resource', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['oauth_provider.Resource'])),
+            ('scope', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['oauth_provider.Scope'], null=True, blank=True)),
             ('verifier', self.gf('django.db.models.fields.CharField')(max_length=10)),
             ('callback', self.gf('django.db.models.fields.CharField')(max_length=2083, null=True, blank=True)),
             ('callback_confirmed', self.gf('django.db.models.fields.BooleanField')(default=False)),
         ))
-        db.send_create_signal('oauth_provider', ['Token'])
+        db.send_create_signal(u'oauth_provider', ['Token'])
 
 
     def backwards(self, orm):
-        
         # Deleting model 'Nonce'
-        db.delete_table('oauth_provider_nonce')
+        db.delete_table(u'oauth_provider_nonce')
 
-        # Deleting model 'Resource'
-        db.delete_table('oauth_provider_resource')
+        # Deleting model 'Scope'
+        db.delete_table(u'oauth_provider_scope')
 
         # Deleting model 'Consumer'
-        db.delete_table('oauth_provider_consumer')
+        db.delete_table(u'oauth_provider_consumer')
 
         # Deleting model 'Token'
-        db.delete_table('oauth_provider_token')
+        db.delete_table(u'oauth_provider_token')
 
 
     models = {
-        'auth.group': {
+        u'auth.group': {
             'Meta': {'object_name': 'Group'},
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
-            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
         },
-        'auth.permission': {
-            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+        u'auth.permission': {
+            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
             'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
         },
-        AUTH_USER_MODEL: {
-            'Meta': {'object_name': AUTH_USER_MODEL.split('.')[-1]},
+        u'auth.user': {
+            'Meta': {'object_name': 'User'},
             'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
             'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
             'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
-            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
             'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
             'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
             'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
-            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
             'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
         },
-        'contenttypes.contenttype': {
+        u'contenttypes.contenttype': {
             'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
             'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
         },
-        'oauth_provider.consumer': {
+        u'oauth_provider.consumer': {
             'Meta': {'object_name': 'Consumer'},
-            'description': ('django.db.models.fields.TextField', [], {}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'key': ('django.db.models.fields.CharField', [], {'max_length': '256'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
             'secret': ('django.db.models.fields.CharField', [], {'max_length': '16', 'blank': 'True'}),
             'status': ('django.db.models.fields.SmallIntegerField', [], {'default': '1'}),
-            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['%s']" % AUTH_USER_MODEL, 'null': 'True', 'blank': 'True'})
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['auth.User']", 'null': 'True', 'blank': 'True'}),
+            'xauth_allowed': ('django.db.models.fields.BooleanField', [], {'default': 'False'})
         },
-        'oauth_provider.nonce': {
+        u'oauth_provider.nonce': {
             'Meta': {'object_name': 'Nonce'},
             'consumer_key': ('django.db.models.fields.CharField', [], {'max_length': '256'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'key': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'timestamp': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
             'token_key': ('django.db.models.fields.CharField', [], {'max_length': '32'})
         },
-        'oauth_provider.resource': {
-            'Meta': {'object_name': 'Resource'},
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+        u'oauth_provider.scope': {
+            'Meta': {'object_name': 'Scope'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'is_readonly': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
             'url': ('django.db.models.fields.TextField', [], {'max_length': '2083'})
         },
-        'oauth_provider.token': {
+        u'oauth_provider.token': {
             'Meta': {'object_name': 'Token'},
             'callback': ('django.db.models.fields.CharField', [], {'max_length': '2083', 'null': 'True', 'blank': 'True'}),
             'callback_confirmed': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'consumer': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['oauth_provider.Consumer']"}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'consumer': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['oauth_provider.Consumer']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'is_approved': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'key': ('django.db.models.fields.CharField', [], {'max_length': '32', 'null': 'True', 'blank': 'True'}),
-            'resource': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['oauth_provider.Resource']"}),
+            'scope': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['oauth_provider.Scope']", 'null': 'True', 'blank': 'True'}),
             'secret': ('django.db.models.fields.CharField', [], {'max_length': '16', 'null': 'True', 'blank': 'True'}),
-            'timestamp': ('django.db.models.fields.IntegerField', [], {'default': '1327884735L'}),
+            'timestamp': ('django.db.models.fields.IntegerField', [], {'default': '1382642717L'}),
             'token_type': ('django.db.models.fields.SmallIntegerField', [], {}),
-            'user': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'tokens'", 'null': 'True', 'to': "orm['%s']" % AUTH_USER_MODEL}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'oauth_tokens'", 'null': 'True', 'to': u"orm['auth.User']"}),
             'verifier': ('django.db.models.fields.CharField', [], {'max_length': '10'})
         }
     }
